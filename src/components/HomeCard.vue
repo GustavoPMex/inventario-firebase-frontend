00a15cc3ddcb86210f8b3c13ec85b36539c656a7
Gustavo Mex <gustavoppymex@gmail.com>
2022-02-07 18:51:10 -0600

    MODIFICA EL ICONO DEL HOMECARD **MODIFICACIONES AL HOMECARD EN SU ICONO

--- src/components/HomeCard.vue
+++ src/components/HomeCard.vue
@@ -5,13 +5,11 @@
         :to="{name: 'TallerList'}"
     >
         <div class="div-content-a">
-            <i v-if="card.count" class="fas fa-exclamation-triangle  icon-content-a"></i>
-            <i v-else class="fas fa-check-circle  icon-content-a"></i>
-
+            <i class="fas fa-tools  icon-content-a"></i>
             <p class="p-content-a mt-3">{{card.name}}</p>
 
             <p v-if="card.count > 1" class="p-content-a mt-3">{{card.count}} Pendientes</p>
-            <p v-else-if="card.count == 1" class="p-content-a mt-3">{{card.count}} Pendiente</p>
+            <p v-else-if="card.count === 1" class="p-content-a mt-3">{{card.count}} Pendiente</p>
             <p v-else class="p-content-a mt-3">Sin Pendientes</p>
         </div>
     </router-link>
